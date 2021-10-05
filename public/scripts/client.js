13b1d563a95e2bc109b438420f5fbb91ee4af932
Jordan Cronier <jcronier777@gmail.com>
2021-10-05 04:15:43 +0000

    update live character counter with jQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,8 +3,3 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-const charUpdate = function(str) {
-  const len = 140 - str.length;
-  document.getElementById("counter").innerHTML = len;
-};
